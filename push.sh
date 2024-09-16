94c53476f73fff9192b0d7b8f40fbfff3e81ca39
tb <torbjornjohannsen@gmail.com>
2024-09-16 09:59:43 +0200

    comments in push.sh

--- push.sh
+++ push.sh
@@ -1,24 +1,32 @@
-#Created with ChatGPT 
+#Created with ChatGPT, with some modifications and own comments for understanding
 
 # Check if an argument is passed, and validate it
-if [ -z "$1" ]; then
-    pType='patch'
+if [ -z "$1" ]; then # if no argument is passed
+    pType="patch" # we default to incrementing the patch(last digit)
 else
-    pType =$1
+    pType="$1" #if we have an argument, then that's the patchtype we use
 fi
 
 # Fetch the latest tags from the remote
 git fetch --tags
 
 # Get the latest tag in the format vx.y.z
-latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`) 
+# "git rev-list --tags --max-count=1" gives us the commit hash of the latest tag in our tree
+# "git describe --tags" with the previous command passed in gives us the name of the tag associated with that commit
 
 if [ -z "$latest_tag" ]; then
   # If no tags exist, start from v0.0.1
   new_tag="v0.0.1"
 else
   # Extract the major, minor, and patch numbers from the latest tag
+  # IFS is the Internal Field Seperator, which we set to seperate by '.'
+  # read reads input from the string provided right of <<< 
+  # -r makes it ignore backslashes and -a makes it throw the results into an array(called version_parts)
+  # "${latest_tag//v/}", the //v/ globally removes all occurences of v(so getting rid of the v in v1.2.3)
   IFS='.' read -r -a version_parts <<< "${latest_tag//v/}"
+
+  # then we just grab the numbers from the array into respective variables
   major="${version_parts[0]}"
   minor="${version_parts[1]}"
   patch="${version_parts[2]}"
@@ -38,7 +46,7 @@ else
       patch=$((patch + 1))
       ;;
     *)
-      echo "Error: Invalid argument. Use 'major', 'minor', or 'patch'."
+      echo "Error: Invalid argument. Use 'major', 'minor', 'patch' or provide no argument, in which case it defaults to patch."
       exit 1
       ;;
   esac
@@ -47,10 +55,10 @@ else
   new_tag="v$major.$minor.$patch"
 fi
 
-# Create the new tag
+# Create the new tag in git
 git tag "$new_tag"
 
-# Push the tag to the remote repository (only the new tag)
+# Push the new tag(ahead of the commit, since github will associate the first commit after a tag is pushed with that tag)
 git push origin "$new_tag"
 
 # Push the current commit to the remote branch (usually main or master)
